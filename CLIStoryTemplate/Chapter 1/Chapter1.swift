f0f3c98ba5d21d5edc5b99dcbc1b44a49e6f0391
Tameikat92 <meekestmeek@gmail.com>
2023-10-18 20:45:30 -0400

    added simple code as practice in chapter 1

--- CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -7,4 +7,7 @@ import Foundation
 
 func chapterOne() {
     // Your portion of the story goes here
+    print("Hello, world!") // printed something simple
+    print(1, 2, 3, 4, 5) //added numbers for practice
+
 }
