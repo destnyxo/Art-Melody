19d639fef404ba34080f75699d73c2a797255d28
Tameikat92 <meekestmeek@gmail.com>
2023-10-20 11:42:00 -0400

    added temperature and also some story notes

--- CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -9,15 +9,21 @@ let storyTitle: String = "Chem Tales"
 let characters = ["Wallace Doubine"]
 let firstArea = "Earth"
 let secondArea = "Space"
-let disaster = "chemtrails"
+let disaster1 = "chemtrails"
+let disaster2 = "Global Warming"
 let villian1 = "marauding bands of outlaws"
+var temperature: Int = 62
 
 func chapterOne() {
     print(storyTitle)
     print("In a not so distant future, two distinct societies emerged from \(firstArea)")
-    print("The planet had descended into chaos, where lawlessness reigned and the land had become treacherous because of \(disaster) wiping through \(firstArea) causing famines, war, and illness. Those who managed to eke out an existence did so through their unique skills, be it")
+    print("The planet had descended into chaos, where lawlessness reigned and the land had become treacherous because of \(disaster1) wiping through \(firstArea) causing famines, war, and illness. Those who managed to eke out an existence did so through their unique skills, be it")
           
     print("farming the barren soil or defending their meager settlements from \(villian1).The vast majority of humanity had fled the planet's strife, taking refuge in the vast expanse of space, where they had built thriving colonies and advanced civilizations.")
+    // create a planet name . Speak about how certain groups were stolen or settled on planet __ to recolonize and make the ship more habitable
+    //I want to create a else if statement and also also find a way to integrate the use of integers.
+    
+    
 
   
 }
