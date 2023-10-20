bd30db176491c4243cc76fc152f85f66483d5b00
Tameikat92 <meekestmeek@gmail.com>
2023-10-20 11:23:40 -0400

    Added a villain 1 to my datatypes.

--- CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -10,13 +10,14 @@ let characters = ["Wallace Doubine"]
 let firstArea = "Earth"
 let secondArea = "Space"
 let disaster = "chemtrails"
+let villian1 = "marauding bands of outlaws"
 
 func chapterOne() {
     print(storyTitle)
     print("In a not so distant future, two distinct societies emerged from \(firstArea)")
-    print("The planet had descended into chaos, where lawlessness reigned and the land had become treacherous because of \(disaster) wiping through \(firstArea). Cuasing famines, war, and illness. Those who managed to eke out an existence did so through their unique skills, be it")
+    print("The planet had descended into chaos, where lawlessness reigned and the land had become treacherous because of \(disaster) wiping through \(firstArea) causing famines, war, and illness. Those who managed to eke out an existence did so through their unique skills, be it")
           
-    print("farming the barren soil or defending their meager settlements from marauding bands of outlaws.The vast majority of humanity had fled the planet's strife, taking refuge in the vast expanse of space, where they had built thriving colonies and advanced civilizations.")
+    print("farming the barren soil or defending their meager settlements from \(villian1).The vast majority of humanity had fled the planet's strife, taking refuge in the vast expanse of space, where they had built thriving colonies and advanced civilizations.")
 
   
 }
