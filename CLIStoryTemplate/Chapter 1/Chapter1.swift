1496360d271a80729dc948687ccd68453f16ec19
Tameikat92 <meekestmeek@gmail.com>
2023-10-20 11:14:44 -0400

    Updated chapter 1

--- CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -4,10 +4,24 @@
 //
 
 import Foundation
+let chapterOneTitle: String = "Emergence"
+let storyTitle: String = "Chem Tales"
+let characters = ["Wallace Doubine"]
+let firstArea = "Earth"
+let secondArea = "Space"
+let disaster = "chemtrails"
 
 func chapterOne() {
-    // Your portion of the story goes here
-    print("Hello, world!") // printed something simple
-    print(1, 2, 3, 4, 5) //added numbers for practice
+    print(storyTitle)
+    print("In a not so distant future, two distinct societies emerged from \(firstArea)")
+    print("The planet had descended into chaos, where lawlessness reigned and the land had become treacherous because of \(disaster) wiping through \(firstArea). Cuasing famines, war, and illness. Those who managed to eke out an existence did so through their unique skills, be it")
+          
+    print("farming the barren soil or defending their meager settlements from marauding bands of outlaws.The vast majority of humanity had fled the planet's strife, taking refuge in the vast expanse of space, where they had built thriving colonies and advanced civilizations.")
 
+  
 }
+
+
+
+// To start the story, call the tellStory function.
+
