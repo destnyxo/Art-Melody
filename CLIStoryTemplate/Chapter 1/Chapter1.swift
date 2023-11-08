f192b13bbd4cab145b45f50a0bc586e21fe4a872
Tameikat92 <meekestmeek@gmail.com>
2023-11-08 00:52:09 -0500

    Added some user inputs.

--- CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -3,7 +3,7 @@
 import Foundation
 
 
-let titles = ["Emergence","Story Intro","Few Are Chosen","Grieving Gaia","War"]
+let titles = ["Emergence","Story Intro","Grieving Gaia","Few Are Chosen","War"]
 var storyTitle: String = "Bridges A Chem Tale"
 var villians = ["marauding bands of outlaws","Space Colonizers" ]
 var characters = ["Wallace Doubine","The Kosen"]
@@ -13,7 +13,7 @@ var temperature: Int = 62
 //var population: Int = 8_000_000_000
 var score = 85
 var wipeoutPopulation = 2_000_000_000
-var CosmonautName = "Shae"
+var CosmonautName = ""
 var USERC: String = ""
 var choice2: Bool = true
 
@@ -24,7 +24,7 @@ func chapterOne() {
 		print("Welcome Cosmonaut. Before we begin, I want to know who you are and what brings you in.")
 		
 		print("Enter your Space Traveling Name:")
-		CosmonautName = "Kiana"
+		CosmonautName = "Lexus"
 		print("Cosmonaut: \(CosmonautName)")
 		
 	}
@@ -46,11 +46,7 @@ func chapterOne() {
 	}
 	earthORSpace()
 	
-	
-	
 	//-------------------------------------------------
-	
-	
 	func chooseArea(){
 		
 		while(choice2) {
@@ -91,34 +87,20 @@ func chapterOne() {
 	//
 	//	} // enums to describe the characters ability and class
 	
-	
-	struct Society {
-		let groups = ["Agriculturalist", "Capitalist", "Anarchist"]
-		let abilities = ["Gifts", "Resourceful", "Guile"]
-		var future: Int
-		let now: Int
-		let moonDistanceFromEarth = 238_900
+//		var future: Int
+//		let now: Int
+//		let moonDistanceFromEarth = 238_900
+//		
+//		mutating func calculateTheFuture() -> Int {
+//			future = moonDistanceFromEarth / moonDistanceFromEarth + now + now // loop and ask until they get it right. variable for is correct . l
+//			return future
+//		}
+//		
+//		init(now: Int) {
+//			self.now = now
+//			self.future = 0
+//		}
 		
-		mutating func calculateTheFuture() -> Int {
-			future = moonDistanceFromEarth / moonDistanceFromEarth + now + now // loop and ask until they get it right. variable for is correct . l
-			return future
-		}
-		
-		init(now: Int) {
-			self.now = now
-			self.future = 0
-		}
-		
-		enum GroupRoles {
-			case agriculturalist
-			case capitalist
-			case anarchist
-		}
-		
-		
-		
-	}
-	
 	func backgroundPoem() {
 		print("""
    In a not so distant future, two distinct societies emerged from \(areas[0])
@@ -126,18 +108,39 @@ func chapterOne() {
    One side bearing gifts
    The other carrying out a curse
    \(areas[1]) doesn't always keep distance
-   A war, when a friend and an enemy have trouble coexisting
+   A war when a friend and an enemy have trouble coexisting
    Keep your eyes focused on what's better when it seems you are headed for the worse
    
    """)
+		let groups = ["Agriculturalist", "Capitalist", "Anarchist"]
+
+		for group in groups {
+			print("\(group):")
+
+			switch group {
+			case "Agriculturalist":
+				print("Abilities: gardening, farming, tending to livestock, community sustainability, cultural cultivation, harmony with nature.")
+			case "Capitalist":
+				print("Originating from Agriculturalist families, Capitalists exploit their skills learned from Agriculturalists for personal gain.")
+				print("Abilities: product promotion, city building, labor recruitment, resourcefulness.")
+			case "Anarchist":
+				print("Anarchists have roots in Agriculturalist practices but aspire to be Capitalists. A hybrid of good and evil, a product of both societies. This group combines skills learned from both groups to achieve their goals and to survive.")
+				print("Abilities: negotiation, guile, charm, self-reliance.")
+			default:
+				print("Unknown group.")
+			}
+
+			print("---")
+		}
+
 	}
 	
 	func performTaskA() {
 		backgroundPoem()
 		
-		var society = Society(now: 2023)
-		let futureYear = society.calculateTheFuture()
-		print("The future year is: \(futureYear)")
+//		var society = Society(now: 2023)
+//		let futureYear = society.calculateTheFuture()
+//		print("The future year is: \(futureYear)")
 	}
 	
 	//enum to talk about 2 groups and how its a third group thats came frim 1 group but aspires to be like the capitalist
@@ -165,9 +168,7 @@ func chapterOne() {
 		print("\(areas[0])'s human population was a sturdy \(originalPop) before the catastrophe struck. \(disasters[1]) impacted life drastically. A three-fourths of human life was wiped out over the course of \(disasters[1]).")
 		
 		
-		let correctPercentage = 75  // Random percentage between 10% and 40%
-		//let percentageReduction = newPop		// Present the multiple-choice scenario to the user
-		
+		let correctPercentage = 75
 		while correctPercentage == 75 {
 			print("Let's estimate the new population after \(disasters[1]) using your math and reading skills. Feel free to use a calculator or google for assistance")
 			print("Based on the information above, what percentage of people were wiped out because of \(disasters[1])?")
@@ -209,6 +210,7 @@ func chapterOne() {
    There was already deforestation,our carbon footprints, and corporate greed
    These \(disasters[0]) caused
    Biodiversity to decrease
+   The plants withered away and animals went astray
    Leaving the \(areas[0]) with a water shortage
    Not even enough for Gaia to have tears to properly grieve
    These \(disasters[0]) caused
@@ -217,28 +219,29 @@ func chapterOne() {
    """
 		)
 		
-		func temp() {
-			temperature = 62
+		func temp(temp: Int) -> String {
 			
-			print("The \(areas[0])'s temperature is \(temperature) degrees.")
+			
+			return("The \(areas[0])'s temperature is \(temp) degrees.")
+			
 		}
 		
-		for _ in 0..<100 { // Simulate 100 time steps
+		for _ in 0..<100 { // Simulate 100 time steps example in playground
 			temperature += 1
 			
 			switch temperature {
 			case 65 :
-				print(" The \(String(describing: temp)) Over farming of fish and chemical dumps has poisoned the water sources.")
+				print("\(temp(temp: temperature))Over farming of fish and chemical dumps have poisoned the water sources.")
 			case 70:
-				print("Over farming has depleted the soil.") // print the temperatures
+				print("\(temp(temp: temperature))Over farming has depleted the soil.")
 			case 75:
-				print("Wars for resources have intensified no one is safe.")
+				print("\(temp(temp: temperature))Wars for resources have intensified no one is safe.")
 			case 80:
-				print("Biodiversity is declining rapidly.")
+				print("\(temp(temp: temperature))Biodiversity is declining rapidly.")
 			case 85:
-				print("Water shortages are becoming critical.")
+				print("\(temp(temp: temperature))Water shortages are becoming critical.")
 			case 90:
-				print("The high carbon footprint is causing environmental harm. Human induced climate change.")
+				print("\(temp(temp: temperature))The high carbon footprint is causing environmental harm. Human induced climate change.")
 			case 95..<100:
 				print("The \(areas[0]) is becoming unlivable.") //looping 5x shows the direness
 			default:
@@ -246,32 +249,182 @@ func chapterOne() {
 			}
 		}
 		
-		print("The \(areas[0])'s temperature has reached \(temperature) degrees. It is now unlivable.")
+		print("The \(areas[0])'s temperature has reached \(temperature) degrees and is now unlivable.")
 		
 	}
+	
+	
 	func performTaskC() {
 		print("Those who could farm and garden were known as \(characters[1]).\(characters[1]) were considered chosen because they were able to create villages where food grew on a land that was decaying .\(characters[1]) had an mystical understanding of the land,using their knowledge to heal the soil, revive ancient farming practices, and cultivate food that sustained their people. Many of the \(characters[1]) had the gift to terraform. They could transform any atmosphere so that it was suitable for supporting human life.")
 		
 		print("Since oxygen was an essential element to keep people who settled in \(areas[1]) alive many of the \(characters[1]) were stolen by \(villians[1]). Some of the issues faced by \(characters[1]) on \(areas[0]) was the \(villians[0]) invading their enclave and robbing them of their livestock, food, and herbs. The \(villians[0]) would also kidnap some of \(characters[1]) to exploit their gifts of knowing how to work with the \(areas[0]). They would enslave them and make them work tirelessly to build what \(characters[1]) had in their villages. The \(villians[0]) would often barter some of \(characters[1]) to the \(villians[1]) for resources on the \(villians[1]) colonizer ships.")
 		
+		characterEncountersMarauders(CosmonautName: CosmonautName)
+		
+		func characterEncountersMarauders(CosmonautName: String) {
+			print("\(CosmonautName), thank you for coming to \(areas[0]) and helping \(characters[1]) feed their animals and tend their land to prepare for this seasons harvest. We are honored to be in your presence!")
+			print("Oh no \(CosmonautName) ! It looks like some \((villians[1])) have gotten in the villiage pretending to be weak travelers in need of our help. They may try to kidnap some more of our people and steal our herbs and livestock.")
+			// Ask the user if they want to take a closer look to check if it's dangerous
+			print("\(CosmonautName), do you think they will get away with our poeple this time? (Yes/No)")
+			if let userInput = readLine(), userInput.uppercased() == "Yes" {
+			// Maybe its dangerous maybe its not
+				let isDangerous = Bool.random()
+				if isDangerous {
+					print("(characters[2]) are stealing our livestock and herbs! \(characters[1]) must fight for their life. They are trying to kidnap our people from the village and force them into submission.")
+				} else {
+					print("They have stolen our livestock and herbs but no one was captured.")
+				}
+			} else {
+				
+				let isDangerous = Bool.random()
+				if isDangerous {
+					print("It turns out \(characters[1]) are dangerous. They are stealing our livestock and herbs! \(characters[1]) must fight for their life.")
+
+					func battleOutcome(kosenStrength: Int, outLawsStrength: Int) -> String {
+						if kosenStrength > outLawsStrength {
+							return "You emerge victorious!"
+						} else {
+							return "You are defeated in battle."
+						}
+					}
+
+					func main() {
+						print("Welcome to The battle of \(characters[1]) and \(villians[1])!")
+						
+						// May be alot
+						let kosenStrength = 75
+						let outLawsStrength = 60
+						
+						print("You, Kosen, have a strength of \(kosenStrength). The outlaws have a strength of \(outLawsStrength).")
+						print("You encounter a group of \(villians[1]) in the woods. What will you do?")
+						print("1. Fight")
+						print("2. Flee")
+						
+						if let choice = readLine() {
+							if choice == "1" {
+								let outcome = battleOutcome(kosenStrength: kosenStrength, outLawsStrength: outLawsStrength)
+								print(outcome)
+								
+								if outcome == "You emerge victorious!" {
+									print("\(CosmonautName), celebrate your victory!")
+								
+								}
+							} else if choice == "2" {
+								print("You decide to flee from \(villians[2]).")
+								print("They will eventually come back, \(CosmonautName)")
+							} else {
+								print("Invalid choice. Please select '1' to fight or '2' to flee.")
+							}
+						}
+					}
+
+					main()
+
+				} else {
+					print("It turns out they weren't dangerous. They have stolen our livestock and herbs but no one was captured.")
+				}
+			}
+		}
+		
 		print("The Kosen's extraordinary gifts held the key to \(areas[0])'s revival and humanity's future among the stars.")
 		
-		characterEncountersMarauders(characterName: CosmonautName)
-		
-		
-	}
-	
-	func characterEncountersMarauders(characterName: String) {
-		print("\(characterName) is on \(areas[0]) helping \(characters[1]) feed their animals and tend their land to prepare for this week's harvest.")
-		
-		let isDangerous = Bool.random() // Debug print to check isDangerous value
-		print("Is it dangerous? \(isDangerous)")
-		
-		if isDangerous {
-			print("They are stealing our livestock and herbs! \(characterName) must fight for their life. They are trying to kidnap our people from the villiage and force them into submission.")
-		} else {
-			print("They have stolen our livestock and herbs but no one was captured.") //They have stolen some of our people and damaged our land
+		for _ in 0...4 {
+			kosenpeopleGifts()
 		}
+		func kosenpeopleGifts()
+		{
+			let randomNum = Int.random(in: 0...4)
+			
+			switch randomNum {
+			case 0:
+				print("gardening")
+			case 1:
+				print("farming")
+			case 2:
+				print("medicine worker")
+			case 3:
+				print("water healers")
+			case 4:
+				print("seer")
+			case 5:
+				print("emotional intelligence")
+			case 6:
+				print("ancestral wisdom")
+			case 7:
+				print("patience")
+			case 8:
+				print("connection with nature")
+			case 9:
+				print("terraform: ability to cleanse the air")
+			case 10:
+				print("honorer of all life")
+			case 11:
+				print("creativity")
+			case 12:
+				print("honoring a higher consciousness")
+			case 13:
+				print("storytelling")
+			default:
+				print("chemist")
+			}
+			
+		}
+
+		
+ print("\(CosmonautName) its time for you to see what gift \(characters[1]) have appointed you based on your cosmic energy!")
+  print("Press 1 to see your \(characters[1]) gift or you can press 0 if you wish to skip.")
+
+  while let input = readLine(), let choice = Int(input) {
+	  if choice == 0 {
+		  print("Thank you for using \(characters[1]) extraordinary gifts program. Goodbye!")
+		  break
+	  } else if choice == 1 {
+		  print("You've received the following Kosen's gift:")
+		  userKosenGift()
+	  } else {
+		  print("Invalid choice. Please press 1 to receive a gift or 0 to exit.")
+	  }
+  }
+
+  func userKosenGift() {
+	
+	  let randomNum = Int.random(in: 0...15)
+	  
+	  switch randomNum {
+	  case 0:
+		  print("gardener")
+	  case 1:
+		  print("farmer")
+	  case 2:
+		  print("holistic healer")
+	  case 3:
+		  print("water healer")
+	  case 4:
+		  print("seer")
+	  case 5:
+		  print("emotional intelligence")
+	  case 6:
+		  print("ancestral wisdom")
+	  case 7:
+		  print("patience")
+	  case 8:
+		  print("ability to connect with all of nature")
+	  case 9:
+		  print("terraformer: ability to cleanse the air")
+	  case 10:
+		  print("honoring all life")
+	  case 11:
+		  print("creative")
+	  case 12:
+		  print("Gods favorite")
+	  case 13:
+		  print("storyteller")
+	 default:
+		  print("chemist")
+	  }
+  }
+
+		
 	}
 	
 	func performTaskD() {
@@ -280,7 +433,7 @@ func chapterOne() {
 		
 	}
 	
-	let choice = "B"
+	let choice = "C"
 	//if USERC == "Earth" {
 	// make user choose A or B
 	
@@ -301,17 +454,5 @@ func chapterOne() {
 	default:
 		print("Invalid choice")
 	}
-	
-	
-	// Continue your story by calling the appropriate function
-	// For example, if you want to proceed with the next part of the story:
-	//performTaskB()
-	
-	
-	//}
-	
-	
-	
-	
 	
 }
